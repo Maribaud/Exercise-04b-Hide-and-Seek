5fbb052bf0066f04714b2de88aa7e753308d8392
Mark Ribaudo <maribaud@iu.edu>
2020-10-13 17:59:51 -0400

    A 2-D platformer

--- Player/Jumping.gd
+++ Player/Jumping.gd
@@ -18,4 +18,6 @@ func physics_process(_delta):
 		player.velocity.y = 0
 		player.velocity += player.jump_power
 		player.move_and_slide(player.velocity, Vector2.UP)
-		SM.set_state("Falling")
+		SM.set_state("Falling") 
+
+
